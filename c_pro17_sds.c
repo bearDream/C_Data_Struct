24180367e92d7f53450ca442864f802cf7b4c057
Lax Zhang <laxzhang@outlook.com>
2019-02-06 12:13:55 +0800

    init random people social network

--- c_pro17_sds.c
+++ c_pro17_sds.c
@@ -4,12 +4,12 @@
 #define PEOPLE 100
 
 typedef struct Node{
-    char data;
-    Node *next;
+    char data[20];
+    struct Node *next;
 }Node;
 
 typedef struct NodeList{
-    Node *head;
+    struct Node *head;
 }NodeList;
 
 typedef struct NetWork{
@@ -21,6 +21,8 @@ NetWork *initNetwork();
 void SDS();
 int BFS(Node *);
 
+Node* new_node(char []);
+
 
 /*
  *  六度空间理论(BFS应用) 的验证
@@ -28,21 +30,67 @@ int BFS(Node *);
  *  抽象为： 在一个图中，一个节点寻找另外一个节点仅需要广度优先遍历至多6层即可找到。
  */
 int main(int argc, char const *argv[]){
+    NetWork *g = initNetwork();
+    puts("Init finished.");
+    for(int i = 0; i < g->v; i++){
+        Node *node = g->Array[i].head;
+        printf("%s 的关系网有", node->data);
+        while(node != NULL){
+            printf("->%s ", node->data);
+            node = node->next;
+        }
+        printf("\n");
+    }
     
     return 0;
 }
 
 // 初始化一个网络
 NetWork *initNetwork(){
-    NetWork *g = (NetWork *)malloc(PEOPLE * sizeof(NetWork));
-    for(int i = 0; i < PEOPLE; i++, g++){
-        // 给每个人所认识的好友去一个[1, 20]随机数
-        g->v = get_rand(1, 20);
-        for(int j = 0; j < g->v; j++){
-            // 设置一个随机姓名
-            // g->Array[j] = ;
-        }
-        
+    NetWork *g = (NetWork *)malloc(sizeof(NetWork));
+    g->Array = (NodeList *) malloc(PEOPLE * sizeof(NodeList));
+    g->v = PEOPLE;
+    for(int i = 0; i < PEOPLE; i++){
+        g->Array[i].head = NULL;
     }
     
+    
+    str s[100];
+    FILE *file = NULL;
+    file = fopen("./sample_of_name.txt", "r");
+    if (file == NULL) {
+        puts("can not open file. check name file.");
+        exit(-1);
+    }
+    read_file(file, s, PEOPLE);
+    puts("read_file was finished");
+    int cu_people, count = 0; // 取姓名样本的计数器
+    Node *node;
+    // 设置网络中的人数
+    for(int i = 0; i < PEOPLE; i++){
+        g->Array[i].head = new_node(s[count].string);
+        count++;
+    }
+
+    int rand;
+    // 设置随机关系网
+    for(int j = 0; j < g->v; j++){
+        cu_people = get_rand(1, 20);
+        Node *node = g->Array[j].head;
+        for(int k = 0; k < cu_people; k++){
+            rand = get_rand(1, PEOPLE-1);
+            node->next = g->Array[rand].head;
+            node = node->next;
+        }
+        node->next = NULL;
+    }
+    
+    return g;    
+}
+
+Node* new_node(char data[]){
+    Node *node = (Node*) malloc(sizeof(Node));
+    strcpy(node->data, data);
+    node->next = NULL;
+    return node;
 }
