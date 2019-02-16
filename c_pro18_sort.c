522d6f2b54e423543f41350c57dc63af314c9650
Lax Zhang <laxzhang@outlook.com>
2019-02-16 11:34:10 +0800

    different simple sort

--- c_pro18_sort.c
+++ c_pro18_sort.c
@@ -5,23 +5,50 @@
 #define RAND_LOWER 1
 
 void *init_array(int[], int); // 生成随机数组
+void *selection_sort(int [], int); // 选择排序
 void *bubble_sort(int [], int); // 冒泡排序
 void *insert_sort(int [], int); // 插入排序
+void *shell_sort(int [], int); // 希尔排序
+void *heap_sort(int [], int); // 堆排序
 
+void swap(int *, int *);
 void print_array(int [], int size);
+int find_min(int [], int, int);
 
 int main(int argc, char const *argv[]){
     int size = 10;
     int arr[size];
+    // 初始化随机数据
     init_array(arr, size);
     puts("orginal array:");
     print_array(arr, size);
 
-    insert_sort(arr, size);
+    // 选择排序
+    // selection_sort(arr, size);
+    // puts("after selection sort:");
+    // print_array(arr, size);
+
+    // 插入/冒泡排序
+    bubble_sort(arr, size);
     puts("after bubble sort:");
     print_array(arr, size);
     
+    // 希尔排序
+    // shell_sort(arr, size);
+    // puts("after shell sort:");
+    // print_array(arr, size);
     return 0;
+}
+
+void *selection_sort(int arr[], int size){
+    int min_pos;
+    for(int i = 0; i < size; i++){
+        min_pos = find_min(arr, i, size);
+        if (min_pos == i) continue;
+        // 交换两数
+        swap(&arr[i], &arr[min_pos]);
+    }
+    return arr;
 }
 
 void *bubble_sort(int array[], int size){
@@ -31,9 +58,7 @@ void *bubble_sort(int array[], int size){
         for(int j = 0; j < i; j++){
             if (array[j] > array[j+1]) {
                 // 交换两数
-                array[j] = array[j]^array[j+1];
-                array[j+1] = array[j+1] ^ array[j];
-                array[j] = array[j] ^ array[j+1];
+                swap(&array[j], &array[j+1]);
                 flag = false;
             }
         }
@@ -53,11 +78,47 @@ void *insert_sort(int array[], int size){
     return array;
 }
 
+// 希尔排序，主要取决于步长
+// 采用Sedgewick增量序列  (1, 5, 19, 41, 109...)  (9 * 4^i - 9 * 2^i + 1)
+//  s = 4^i - 3* 2^i + 1; s = 9*4^i - 9*2^i + 1;  
+void *shell_sort(int array[], int size){
+
+    printf("shell----\n");
+    int tmp, k;
+    for(int D = size/2; D > 0; D/=2){ // 设步长
+        // 插入排
+        for(int j = D; j < size; j++){
+            tmp = array[j];
+            for(k = j; k >=D && array[k-D] > tmp; k-=D)
+                array[k] = array[k-D];
+            array[k] = tmp;
+        }
+    }
+    return array;
+    
+}
+
 void *init_array(int array[], int size){
     for(int i = 0; i < size; i++){
         array[i] = get_rand(RAND_LOWER, RAND_UPPER);
     }
     return array;
+}
+
+void swap(int *a, int *b){
+    *a = *a ^ *b;
+    *b = *b ^ *a;
+    *a = *a ^ *b;
+}
+
+int find_min(int array[], int i, int size){
+    int min_index = i;
+    for(int j = i; j < size; j++){
+        if (array[j] < array[min_index]) {
+            min_index = j;
+        }
+    }
+    return min_index;
 }
 
 void print_array(int arr[], int size){
